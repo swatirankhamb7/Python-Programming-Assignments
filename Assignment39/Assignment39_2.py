84901550673a521ad2c648b272246a347812defa
swatirankhamb7 <132763196+swatirankhamb7@users.noreply.github.com>
2026-03-05 21:08:20 +0530

    Add files via upload

---
+++ Assignment39/Assignment39_2.py
@@ -0,0 +1,96 @@
+"""8. Write a single structured Python program that performs:
+Dataset loading
+Data analysis
+Visualization
+Train-test split
+Model training
+Prediction
+Accuracy calculation
+Confusion matrix generation
+Final conclusion
+Your code should include proper comments explaining each step."""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns 
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+import numpy as np
+from sklearn.metrics import (accuracy_score,confusion_matrix,classification_report,ConfusionMatrixDisplay)
+
+def main():
+    boarder="-"*50
+    #step 1 : Dataset loading
+    print("Step 1 : load the dataset")
+    df=pd.read_csv("student_performance_ml.csv")
+    print("Dataset Successfully loaded.......!")
+    print(boarder)
+
+    #step 2 : Dataset Analysis
+    print("Step 2 : Dataset Analysis")
+    #print(df)
+    print("Shape of Dataset : ",df.shape)
+    print("Column names : ",list(df.columns))
+    print()
+    print("First Five Records : ")
+    print(df.head())
+    print()
+    print("Last five Records :")
+    print(df.tail())
+    print("Null Values are : ",df.isnull().sum())
+    print(boarder)
+
+   #Visualization
+    print("Step 3 : Data Visualization : ")
+    sns.scatterplot(x=df["StudyHours"],y=df["FinalResult"],hue=df["FinalResult"])
+    plt.title("Relationship between Study Hours and Final Result")
+    plt.xlabel("Study Hours")
+    plt.ylabel("Final Result")
+    plt.show()
+
+    #Train_test_split
+    List=["StudyHours",
+         "Attendance", 
+         "PreviousScore",
+         "AssignmentsCompleted",
+         ]
+    X=df[List]
+    Y=df["FinalResult"]
+    print(boarder)
+
+    print("step 4 : Train test split")
+    x_train,x_test,y_train,y_test=train_test_split(X,Y,test_size=0.2,random_state=42)
+    print("Size of X train : ",x_train.shape)
+    print("Size of X test : ",x_test.shape)
+    print("Size of Y train : ",y_train.shape)
+    print("Size of Y test : ",y_test.shape)
+    print(boarder)
+
+    #Model training
+    print("Step 5 : Model Traning")
+    model=DecisionTreeClassifier(max_depth=100)
+    model.fit(x_train,y_train)
+    y_pred=model.predict(x_test)
+    print(boarder)
+
+    #Accuracy Calculation : To Evaluate the performance of model
+    print("Step 6 : Accuracy Calculation")
+    accuracy=accuracy_score(y_test,y_pred)
+    print("Accuracy Score  : ",accuracy*100)
+    print(boarder)
+
+    #Confusion matrix generation
+    print("Step 7 : Confusion matrix generation")
+    matrix=confusion_matrix(y_test,y_pred)
+    print(matrix)
+
+    """
+    Final Conclusion : 
+    Here We used Supervised Machine Learning algorithm Decision tree Classifer
+    because Given data is labled data.
+    our Given model is good fitted now.
+    The model Accuracy is 100%.
+    """
+
+if __name__=="__main__":
+    main()
